7b555a8cd763928745ad88e94cb8e13b954da477
LAPTOP-HINUMAAJ\Loki <thiagokoster@gmail.com>
2018-05-12 21:44:48 -0300

    Regex changes
    Now only look for pattern on the beginning of row.

--- fileHandling.pl
+++ fileHandling.pl
@@ -1,18 +1,38 @@
+use warnings; use strict;
+
 my $filename = 'posts.html';
+
 
 open(my $fh, '<',$filename) or 
     die "Could not open file '$filename' '$!'";
 
+
+
+
+#push @products, [(0)x$numberOfLines] for (0..$numberOfLines);   #id store name
+
 while(my $row = <$fh>)
 {
-    #Return everything inside [ ] or ( )
+    #Return everything inside [ ] or ( ) after first whitespace
     #[] is $1 and () is $2
-    if($row =~ /\[(.*?)\]|\((.*?)\)/g)
+
+    
+    while($row =~ /^\S\s\"\[(.*?)\]|^\S\s\"\((.*?)\)/gi)
     {
-        print "$2\n";
+        #$products[$index][0] = $row =~ /(.*?)/;
+        if($1)
+        {            
+            print "$1\n";
+         #   $products[$index][1] = $1;
+        }
+        if($2)
+        {
+            print "$2\n";
+          #  $products[$index][1] = $1;                 
+        }
     } 
 }
 
-
 print "Done\n";
+#print $products[1][0];
 close $fh;
