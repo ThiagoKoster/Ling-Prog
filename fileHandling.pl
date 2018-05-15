baf1d1a47b663fdf5ebcf894f75111d724a1891c
LAPTOP-HINUMAAJ\Loki <thiagokoster@gmail.com>
2018-05-15 02:15:18 -0300

    Creation of subrotines
    Now most of the parameters are found by subrotines. Utilization of the Text::Table module

--- fileHandling.pl
+++ fileHandling.pl
@@ -1,35 +1,77 @@
+use Text::Table;
 use warnings; use strict;
 
+my @promoMatrix ; # [ [Id0,Store,Product,Price] , .... , [IdN,Store,Product,Price] ] 
 my $fileName = 'posts.html';
-
 open(my $fileHandler, '<',$fileName) or 
     die "Could not open file '$fileName' '$!'";
 
-my @promoMatrix ; # [ [Id0,Store,Product,Price] , .... , [IdN,Store,Product,Price] ] 
+#Subrotines
+
+#The subrotines find each element and then returns the row without them.
+sub printProducts
+{
+    my $table = Text::Table ->new("Id", "Store", "Product", "Price");
+    for my $i (0 .. $#promoMatrix)
+    {
+        $table->load(
+            [$promoMatrix[$i][0], $promoMatrix[$i][1], $promoMatrix[$i][2], $promoMatrix[$i][3]]
+        );
+    }
+
+    print $table;
+}
+
+sub findIds
+{
+    my($count,$row) = @_;
+    $row =~ s/(^\w*)//gi;
+    $promoMatrix[$count][0] = $&;
+    return $row;
+    
+}
+
+sub findStores 
+{
+    my ($count,$row) = @_;
+    $row =~ s/^\S*\s\"\[(.*?)\]|^\S*\s\"\((.*?)\)//i;
+    $promoMatrix[$count][1] = $+;
+    return $row;
+}
+
+sub findPrices
+{
+    my ($count,$row) = @_;
+    $row =~ s/\.//;
+    $row =~ s/R\$\s*(\d+[,]*\d{1,9})//;
+    $promoMatrix[$count][3] = $+;
+    return $row;
+}
+
+
+#Subrotines end
 
 my $count = 0 ;
 while(my $row = <$fileHandler>)
 {
     #Return everything inside [ ] or ( ) after first whitespace
     #my ($store) = ($row =~ /^\d\s\"[\[|\(](.*?)[\]|\)]/gi );
-    $row =~ /^\S\s\"\[(.*?)\]|^\S\s\"\((.*?)\)/gi ;
-    my $store = $+ ; # $+ = last match
+    #$row =~ /^\S\s\"\[(.*?)\]|^\S\s\"\((.*?)\)/gi ;
+    
+    $row = findIds($count,$row);
+    $row = findStores($count, $row);
+    $row = findPrices($count,$row);
+    
+    #removeWords($row,@words);
     # \G continues regex search from where the last match ocurred
-    my ($product) = ($row =~ /\G(.*?)(R\$|\d+\,\d+)/i ); # R$ or numbers followed by . or ,
-    my ($price) = ($row =~ /(R\$\s*\d+[\.?|\,?]\d*\,?)/); # BUG  ===== R$ 2060 doesnt work, why? shouldnt \d* treat that?
-
-    $promoMatrix[$count][0] = $count;  # id?
-    $promoMatrix[$count][1] = $store;
+    #my ($product) = ($row =~ /\G(.*?)(R\$|\d+\,\d+)/i ); # R$ or numbers followed by . or ,
+    my $product = $row;
     $promoMatrix[$count][2] = $product;
-    $promoMatrix[$count][3] = $price;
     ++$count ;
 }
 
- for my $i ( 0 .. $#promoMatrix ) { # $#protoMatrix = size of protoMatriz
-     print "\t [ @{$promoMatrix[$i]} ],\n";
- }
+printProducts();
 
 
 print "Done\n";
-#print $products[1][0];
 close $fileHandler;
