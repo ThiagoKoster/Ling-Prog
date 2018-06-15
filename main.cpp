abd473dd78ed6995b30e62126872c707b9541b58
Thiago Koster Lago <thiagokoster@gmail.com>
2018-06-15 20:42:52 -0300

    Twitter authentication almost working...

--- main.cpp
+++ main.cpp
@@ -1,50 +1,99 @@
+#include "Base64.h"
+#include<string>
 #include<iostream>
-#include<fstream>
-#include<twitcurl.h>
-
+#include<curl/curl.h>
+#include <json.hpp>
+#include <algorithm>
 using namespace std;
+using std::replace;
+using Json = nlohmann::json;
 
-void getKeys(string &consumerK,string &consumerS,string &accessTokenK,string &accessTokenS )
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-    ifstream keysFile;
-    string line;
-    keysFile.open("keys.txt");
-    getline(keysFile,consumerK);
-    getline(keysFile,consumerS);
-    getline(keysFile,accessTokenK);
-    getline(keysFile,accessTokenS);
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
 }
 
-int main(int argc, char const *argv[])
+string GetAccessToken(string key,string secret)
 {
- 
-  twitCurl twitter;
-  string consumerKey ;
-  string consumerSecret ;
-  string myOAuthAccessTokenKey ;
-  string myOAuthAccessTokenSecret;
+     string strToEncode =  (key + ":" + secret).c_str();
+    const unsigned int lenght = key.length() + secret.length() + 1;
+    
 
-  getKeys(consumerKey,consumerSecret,myOAuthAccessTokenKey,myOAuthAccessTokenSecret);
-    cout << consumerKey << endl;
-    cout << consumerSecret << endl;
-    cout << myOAuthAccessTokenKey << endl;
-    cout << myOAuthAccessTokenSecret << endl;
+    string basicAuth = base64_encode((const unsigned char*) strToEncode.c_str(),lenght);
+    const char* headerParam = ("Authorization: Basic " + basicAuth).c_str();
+    struct curl_slist *headers=NULL; // init to NULL is important 
+    headers = curl_slist_append(headers, headerParam);  
+    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded;charset=UTF-8");
+    CURL *curl = curl_easy_init();
 
-  twitter.getOAuth().setConsumerKey(consumerKey);
-  twitter.getOAuth().setConsumerSecret(consumerSecret);
-  twitter.getOAuth().setOAuthTokenKey(myOAuthAccessTokenKey);
-  twitter.getOAuth().setOAuthTokenSecret(myOAuthAccessTokenSecret);
+    string readBuffer;
+    Json outputJson;
+    if(curl){
+        curl_easy_setopt(curl,CURLOPT_URL,"https://api.twitter.com/oauth2/token");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
+        curl_easy_setopt(curl, CURLOPT_POST, 1);
+        readBuffer.clear();
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        
+        curl_easy_perform(curl);
+    }
+    curl_easy_cleanup(curl);
+    curl_slist_free_all(headers);
+    headerParam = nullptr;
+    return readBuffer;
+}
 
-  string tweetTest = "Hello World!";
-  string replyMsg;
-  if(twitter.statusUpdate(tweetTest))
-  {
-    twitter.getLastWebResponse(replyMsg);
-    printf("\ntwitterClient::twitcurl::statusUpdate web response:\n%s\n",replyMsg.c_str());
-  }else{
-    twitter.getLastCurlError(replyMsg);
-    printf("\ntwitterClient::twitcurl::statusUpdate ERROR:\n%s\n",replyMsg.c_str());
-  }
+string GetTweets(string accessToken)
+{
+    struct curl_slist *headers=NULL; // init to NULL is important 
+    CURL *curl = curl_easy_init();
 
-  return 0;
+    string readBuffer;
+    Json outputJson;
+    
+    
+    if(curl) {
+    //cout << headerParam << endl;
+    headers = curl_slist_append(headers, "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAB5b6gAAAAAAa%2FiLGU3x5csgAxNoDdU%2BrULystQ%3DJT8UqfSNd8gZq5DSil93IT4UTvn7dbqVKYokLu5VJdJvK1V9gi"); 
+    
+    //TODO: POR ALGUM MOTIVO USANDO headerParam NÃO FUNCIONA...
+    //const char* headerParam = ("Authorization: Bearer " + accessToken).c_str();
+    //headers = curl_slist_append(headers,headerParam); 
+    
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(curl, CURLOPT_HTTPGET,1);
+    readBuffer.clear();
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    curl_easy_perform(curl);
+    
+    }
+      
+    curl_easy_cleanup(curl);
+    curl_slist_free_all(headers);
+    return readBuffer;
+}
+
+int main()
+{
+    string consumerKey = "idNDUtlKDmWG0TPC0Hnp9MQmJ";  
+    string consumerSecret = "MIL5EPvTickAVPifHXgsEfIBOnskatFIEooiWZZBjMrvLMVSsw";
+       
+    Json accessTokenJSON = Json::parse(GetAccessToken(consumerKey,consumerSecret));
+    string accessToken = accessTokenJSON["access_token"];
+
+    //Remover \" da string
+    accessToken.erase(std::remove(accessToken.begin(), accessToken.end(), '\"'), accessToken.end());
+
+    cout << accessToken << endl;
+    Json tweets = Json::parse(GetTweets(accessToken));
+
+    cout << tweets << endl;
+    
+   
+    return 0;
 }
