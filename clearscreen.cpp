fb0579ea93ee468f3fd2afba75b6ceab07428eb8
Anor Neto <anornetoo@gmail.com>
2018-06-16 03:07:35 -0300

    created main_menu.cpp to test menu functions and clearscreen function

---
+++ clearscreen.cpp
@@ -0,0 +1,53 @@
+#if defined(_WIN32) // if windows
+    #include <windows.h>
+#else // else = posix
+    #include <unistd.h>
+    #include <term.h>
+    #include <iostream>
+#endif
+
+void clearScreen(){
+    #if defined(_WIN32)
+        HANDLE                     hStdOut;
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        DWORD                      count;
+        DWORD                      cellCount;
+        COORD                      homeCoords = { 0, 0 };
+
+        hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+        if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+        /* Get the number of cells in the current buffer */
+        if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
+        cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+        /* Fill the entire buffer with spaces */
+        if (!FillConsoleOutputCharacter(
+            hStdOut,
+            (TCHAR) ' ',
+            cellCount,
+            homeCoords,
+            &count
+            )) return;
+
+        /* Fill the entire buffer with the current colors and attributes */
+        if (!FillConsoleOutputAttribute(
+            hStdOut,
+            csbi.wAttributes,
+            cellCount,
+            homeCoords,
+            &count
+            )) return;
+
+        /* Move the cursor home */
+        SetConsoleCursorPosition( hStdOut, homeCoords );
+    #else
+        if (!cur_term)
+        {
+        int result;
+        setupterm( NULL, STDOUT_FILENO, &result );
+        if (result <= 0) return;
+        }
+        putp( tigetstr( "clear" ) );
+    #endif
+}
---
+++ main_menu.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Menu.h"
+#include "clearscreen.h"
+
+using namespace std;
+
+int main(void)
+{
+    Menu menu;
+    
+    clearScreen();
+    while( menu.getMenuOption() != 0){
+        menu.printMenu();
+        menu.askMenuOption();
+        menu.callFunction();
+        cin.ignore();
+        cin.get();
+        clearScreen();
+    }
+
+}
